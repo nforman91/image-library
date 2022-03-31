87aa042985715ec87b71ec8b1058e84bac2fe2f8
Nathan <nathan.forman@gmail.com>
2022-03-30 17:42:48 -0700

    Added URL

--- image-library/src/components/Modal.js
+++ image-library/src/components/Modal.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './Modal.css';
 import axios from 'axios';
+import Star from './Star';
+// import empty_star from '../logos/empty_star.png';
+// import full_star from '../logos/full_star.png';
 
 const Modal = (props) => {
     const {team} = props
     const [selectedTeamCard, setselectedTeamCard] = useState(null);
+    const [star, setStar] = useState(false)
 
     useEffect(() => {
         axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${team.id}`)
@@ -29,7 +33,12 @@ const Modal = (props) => {
                     <h4>Conference: {selectedTeamCard.conference.name}</h4><br/>
                     <h4>Division: {selectedTeamCard.division.name}</h4><br/>
                     <h4>City: {selectedTeamCard.venue.city}</h4><br/>
-                    <h4>Stadium: {selectedTeamCard.venue.name}</h4>
+                    <h4>Stadium: {selectedTeamCard.venue.name}</h4><br/>
+                    <h4>Official Site URL: {selectedTeamCard.officialSiteUrl}</h4>
+                    <h4 onClick={() => setStar(!star)}>Favorite?</h4>
+                    {star && <Star star={star} setStar={setStar}/>}
+                    {/* <h6>{star}</h6>
+                    <img className="star" src={star} alt="Star for favorite team" onClick={() => setStar(!star)}/> */}
                 </div>
             </div>
     }
