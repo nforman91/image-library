5511a8e6af1d71682ba0008a8756ba3d2a9fb7dd
Nathan <nathan.forman@gmail.com>
2024-08-12 12:18:14 -0700

    Refreshing logo links 3

--- image-library/src/components/Title.js
+++ image-library/src/components/Title.js
@@ -5,7 +5,7 @@ import Cards from './Cards';
 const Title = () => {
     return (
         <section className='title-container'>
-            <img className='nhl-shield' src="https://en.wikipedia.org/wiki/National_Hockey_League#/media/File:05_NHL_Shield.svg" alt="NHL shield logo"/>
+            <img className='nhl-shield' src="https://media.d3.nhle.com/image/private/t_q-best/prd/assets/nhl/logos/nhl_shield_wm_on_dark_fqkbph" alt="NHL shield logo"/>
             <h2 className='page-title'>NHL Teams</h2>
             <Cards className='cards'/>
         </section>
